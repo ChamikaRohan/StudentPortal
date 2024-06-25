ef613f1228fc61a232869b1157dedfcd0c2ad84b
ChamikaRohan <chamika707@gmail.com>
2024-06-25 18:48:58 +0530

    Created Create-Student-API route and controllers

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
+use App\Http\Controllers\StudentController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Route::get('/get-students', [StudentController::class, 'index']);
